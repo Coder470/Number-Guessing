32298d517c65b6b771f0b319635d3e7164c85655
Coder470 <Coder472.team20@gmail.com>
2025-02-02 19:43:57 +0000

    Guessing-Game.py

--- Guessing-Game.py
+++ Guessing-Game.py
@@ -1,55 +1,60 @@
-
-print("Welcome to the Number Guessing Game!")
-print("I'm thinking of a number between 1 and 100.")
-print("You have 5 chances to guess the correct number.")
-print()
-print()
-
-print("Please select the difficulty level: ")
-print("1. Easy (10 chances)")
-print("2. Medium (5 chances)")
-print("3. Hard (3 chances)")
-attempts = 0
-#Difficulty options
-while True:
-    choice = input("Enter your choice: ")
-    if choice == "1":
-        print()
-        print()
-        print("Great! You have selected the Easy difficulty level.")
-        print("Let's start the game!")
-        attempts = 10
-        break
-    elif choice == "2":
-        print()
-        print()
-        print("Great! You have selected the Medium difficulty level.")
-        print("Let's start the game!")
-        attempts = 5
-        break
-    elif choice == "3":
-        print()
-        print()
-        print("Great! You have selected the Hard difficulty level.")
-        print("Let's start the game!")
-        attempts = 3
-        break
-
-#Generates a number from 1-100
 import random
-number = random.randint(1,100)
-#Gives a specific number of guesses depending on the difficulty you pick.
-Guess = int(input("Enter your guess: "))
-while attempts > 0:
-    if Guess < number:
-        print(f"Incorrect! The number is greater than {Guess}.")
-        attempts -= 1
-    elif Guess > number:
-        print(f"Incorrect! The number is less than {Guess}")
-        attempts -= 1
-    elif Guess == number:
-        print("Congratulations! You guessed the correct number")
-        break
 
-    Guess = int(input("Enter your guess: "))        
+def Welcome_Message():
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+    print("You have 5 chances to guess the correct number.")
+    print()
 
+
+def difficulty_select():
+    print("Please select the difficulty level: ")
+    print("1. Easy (10 chances)")
+    print("2. Medium (5 chances)")
+    print("3. Hard (3 chances)")
+
+    while True:
+        choice = input("Enter your choice: ")
+        if choice == "1":
+            print("\nGreat! You have selected the Easy difficulty level.")
+            print("Let's start the game!")
+            return 10 #Easy mode
+        
+        elif choice == "2":
+            print()
+            print("Great! You have selected the Medium difficulty level.")
+            print("Let's start the game!")
+            return 5 #Medium Mode
+        
+        elif choice == "3":
+            print()
+            print("Great! You have selected the Hard difficulty level.")
+            print("Let's start the game!")
+            return 3   #Hard Mode
+    
+
+def number_guess_generator():
+    number = random.randint(1,100)
+    return number
+
+def guessing(attempts,number):
+    Guess = int(input("Enter your guess: "))
+    while attempts > 0:
+        if Guess < number:
+            print(f"Incorrect! The number is greater than {Guess}.")
+            attempts -= 1
+        elif Guess > number:
+            print(f"Incorrect! The number is less than {Guess}")
+            attempts -= 1
+        elif Guess == number:
+            print("Congratulations! You guessed the correct number")
+            break
+
+        Guess = int(input("Enter your guess: "))
+
+
+#Game execution
+Welcome_Message()
+attempts = difficulty_select()
+number = number_guess_generator()
+guessing(attempts,number)
